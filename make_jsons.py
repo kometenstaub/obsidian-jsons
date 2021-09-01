26fad8bd5b5c5c7af8eb844a9a905de9a3668afa
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2021-09-01 21:29:21 +0200

    url works

---
+++ make_jsons.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+import os
+import json
+import re
+import urllib.parse
+
+for dirpath, dirnames, files in os.walk("./obsidian-docs/en/"):
+    #print(f"Found directory: {dirnames}, located here:{dirpath}")
+    for file_name in files:
+        if file_name.endswith(".md"):
+            normalised_path = os.path.normpath(dirpath + "/" + file_name)
+
+            file_dict : dict = {}
+            url : str = "https://help.obsidian.md/"
+            title : str = ""
+            description : str = ""
+            color : str = ""
+
+            split_path : list = normalised_path.split("/")[2:]
+            unencoded_url_part : str = "/".join(split_path)
+            url += urllib.parse.quote(unencoded_url_part)
+            url = url.replace("%20", "+")
+            print(url)
+
+            print(f"Found file: {file_name}")
+            #print(normalised_path)
+
