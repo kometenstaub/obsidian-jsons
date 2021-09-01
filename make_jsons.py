0486e651d46d54b618f950fa30765b43910244b3
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2021-09-01 23:07:21 +0200

    removed re dependency

--- make_jsons.py
+++ make_jsons.py
@@ -1,11 +1,12 @@
 #!/bin/python3
 import os
 import json
-import re
 import urllib.parse
 
 description_regex = r"^.*(?:\n.*){10}"
 description_regex_small = r"^.*(?:\n.*){5}"
+
+color : int = 3092790
 
 for dirpath, dirnames, files in os.walk("./obsidian-docs/en/"):
     #print(f"Found directory: {dirnames}, located here:{dirpath}")
@@ -17,8 +18,8 @@ for dirpath, dirnames, files in os.walk("./obsidian-docs/en/"):
             url : str = "https://help.obsidian.md/"
             title : str = ""
             description : str = ""
-            color : int = 3092790
 
+            # URL
             split_path : list = normalised_path.split("/")[2:]
             unencoded_url_part : str = "/".join(split_path)
             url += urllib.parse.quote(unencoded_url_part)
@@ -29,28 +30,28 @@ for dirpath, dirnames, files in os.walk("./obsidian-docs/en/"):
             #print(url)
             file_dict["url"] = url
             
+            # title
             title = file_name.replace(".md", "")
             file_dict["title"] = title
             
+            # color
             file_dict["color"] = color
 
             #print(f"Found file: {file_name}")
             #print(normalised_path)
 
-
+            # description
             with open(normalised_path, "r", encoding="utf-8") as f:
-                content = "".join(f.readlines())
-                #print(content)
-                match = re.search(description_regex, content)
+                content : dict = f.readlines()
                 result : str = ""
-                if match:
-                    result = match.group()
+                if len(content) >= 10:
+                    result = "".join(content[:10])
+                    if len(result) >= 2000:
+                        result = result[:2000]
                 else:
-                    new_match = re.search(description_regex_small, content)
-                    if new_match:
-                        result = new_match.group()
-                    else:
-                        result = ""
+                    result = "".join(content)
                 file_dict["description"] = result
 
             #print(file_dict)
+
+            
